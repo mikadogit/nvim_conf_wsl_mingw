3692fc1d4f9be3b19b55dcf78f4acbc0fd4f55b3
mikadogit <108522700+mikadogit@users.noreply.github.com>
2025-04-02 11:30:51 +0200

    Update init.lua : log balise colored

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,3 +21,17 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+vim.cmd [[
+  highlight WarningMsg ctermfg=Yellow guifg=Yellow
+  highlight ErrorMsg ctermfg=Red guifg=Red
+  highlight CriticalMsg ctermfg=Magenta guifg=Magenta
+  highlight InfoMsg ctermfg=Green guifg=Green
+  highlight DebugMsg ctermfg=Blue guifg=Blue
+
+  autocmd BufReadPost,BufWinEnter * lua vim.fn.matchadd('WarningMsg', '\\[warning\\]')
+  autocmd BufReadPost,BufWinEnter * lua vim.fn.matchadd('ErrorMsg', '\\[error\\]')
+  autocmd BufReadPost,BufWinEnter * lua vim.fn.matchadd('CriticalMsg', '\\[critical\\]')
+  autocmd BufReadPost,BufWinEnter * lua vim.fn.matchadd('InfoMsg', '\\[info\\]')
+  autocmd BufReadPost,BufWinEnter * lua vim.fn.matchadd('DebugMsg', '\\[debug\\]')
+]]

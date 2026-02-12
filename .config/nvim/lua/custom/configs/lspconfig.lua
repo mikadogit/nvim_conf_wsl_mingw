4a3c4a23a8bc5e18bebd64f8b8a8a084ceab54e6
mikadogit <108522700+mikadogit@users.noreply.github.com>
2026-02-12 23:36:44 +0100

    Update lspconfig.lua

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -2,8 +2,7 @@ local base = require("plugins.configs.lspconfig")
 local on_attach = base.on_attach
 local capabilities = base.capabilities
 
-local lspconfig = require("lspconfig")
-lspconfig.clangd.setup {
+vim.lsp.config("clangd", {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
     on_attach(client, bufnr)
@@ -15,4 +14,6 @@ lspconfig.clangd.setup {
     "--header-insertion=never",
     "--query-driver=/usr/bin/x86_64-w64-mingw32-g++"
   },
-}
+})
+
+vim.lsp.enable("clangd")
